83e3bf674708153a29c06dbfe9963fed8185900d
Jitao David Zhang <jitao_david.zhang@roche.com>
2016-01-05 11:21:00 +0100

    plot and save FDR simulation results

--- test-camera.R
+++ test-camera.R
@@ -1,4 +1,9 @@
 library(limma)
+library(lattice)
+library(ribiosPlot)
+
+figfile <- function(x) file.path("figures", x)
+outfile <- function(x) file.path("output", x)
 
 set.seed(1887)
 
@@ -40,16 +45,57 @@ fdrAfterAppendRandom <- function(matrix, index, design, nRandom=10) {
     }
     cameraRes <- camera(matrix, index=index, design=design, sort=FALSE)
     if(nRandom==0) {
-        fdr <- cameraRes["index", "PValue"] ## in case of single gene set no FDR correction is made
-        fdrRank <- 1
+        p <- fdr <- cameraRes["index", "PValue"] ## in case of single gene set no FDR correction is made
+        pRank <- fdrRank <- 1
     } else {
+        p <- cameraRes["index", "PValue"]
         fdr <- cameraRes["index", "FDR"]
+        pRank <- sum(cameraRes$PValue<=p)
         fdrRank <- sum(cameraRes$FDR<=fdr)
     }
-    return(list(FDR=fdr, FDRrank=fdrRank))
+    return(list(p=p, pRank=pRank, FDR=fdr, FDRrank=fdrRank))
 }
 
-(nRandoms <- as.integer(c(0, 1,5,10,20,50,10^seq(2,6,0.125))))
+(nRandoms <- as.integer(c(0, 1,5,10^seq(1,4,0.125), 5*10^4,1*10^5,2*10^5)))
 library(multicore)
 randomFDRs <- mclapply(nRandoms, function(x)
     fdrAfterAppendRandom(y, index1, design, nRandom=x))
+randomFDRresult <- data.frame(nRandom=nRandoms,
+                              p=sapply(randomFDRs, function(x) x$p),
+                              pRank=sapply(randomFDRs, function(x) x$pRank),
+                              FDR=sapply(randomFDRs, function(x) x$FDR),
+                              FDRrank=sapply(randomFDRs, function(x) x$FDRrank))
+logSteps <- c(1,2,5,10,50,100,200, 500,1000,
+              2000,5000,10000, 20000, 50000, 1*10^5, 2*10^5)
+xyplot(FDR~nRandom, data=randomFDRresult,
+       abline=list(h=c(log2(1), log2(0.10), log2(randomFDRresult$p[1])),
+           col=c(rep("black", 2), "red"), lwd=c(rep(1,2), 1.5), lty=c(rep(2,2), 1)),
+       main="FDR of a genuine DE-geneset (camera)", 
+       xlab="Number of random gene sets tested", ylab="FDR (Benjamini-Hochberg)",
+       scales=list(tck=c(1,0), alternating=1L,
+           y=list(log=2, at=c(1E-4,1E-3,1E-2,0.02, 0.05, 0.10, 0.25, 0.5, 1)),
+           x=list(at=logSteps,
+               log=TRUE), rot=45), type="b", fill="darkgray", pch=21)
+ipdf(figfile("FDR-numberOfRandomGeneSets.pdf"), width=6L, height=6L)
+
+xyplot(FDRrank~nRandom, data=randomFDRresult,
+       abline=list(a=0,b=1, lty=2),
+       main="FDR rank of a genuine DE-geneset (camera)", 
+       xlab="Number of random gene sets tested", ylab="FDR rank (Benjamini-Hochberg)",
+       scales=list(tck=c(1,0), alternating=1L,
+           y=list(log=10, at=logSteps),
+           x=list(at=c(1,10,50,100,200, 500,1000,
+                      2000,5000,10000, 20000, 50000, 1*10^5, 2*10^5),
+               log=TRUE), rot=45), type="b", fill="darkgray", pch=21)
+ipdf(figfile("FDRrank-numberOfRandomGeneSets.pdf"), width=6L, height=6L)
+
+xyplot(pRank~nRandom, data=randomFDRresult,
+       abline=list(a=0,b=1, lty=2),
+       main="pValue rank of a genuine DE-geneset (camera)", 
+       xlab="Number of random gene sets tested", ylab="pValue rank",
+       scales=list(tck=c(1,0), alternating=1L,
+           y=list(log=10, at=logSteps),
+           x=list(at=c(1,10,50,100,200, 500,1000,
+                      2000,5000,10000, 20000, 50000, 1*10^5, 2*10^5),
+               log=TRUE), rot=45), type="b", fill="darkgray", pch=21)
+ipdf(figfile("pRank-numberOfRandomGeneSets.pdf"), width=6L, height=6L)

2ab929c343470f2402eccc0af3434f831c580eac
zhangj83 <jitao_david.zhang@roche.com>
2016-02-05 22:38:57 +0100

    add random core and make more simulations

--- simulate-data.R
+++ simulate-data.R
@@ -1,20 +1,36 @@
 library(ribiosPlot)
 library(ribiosIO)
+library(ribiosGSEA)
+library(lattice)
+library(latticeExtra)
 figfile <- function(x) file.path("figures", x)
 
-source("simulate-data-funcs.R")
-
-testSimulator <- function(tgSim, pFunc=pFuncCamera) {
-    bench <- newBenchmarker(tgSim, pFunc=pFunc)
+testSimulator <- function(tgSim, pFunc=pFuncCamera, ngsr=99, B=100) {
+    bench <- newBenchmarker(tgSim, ngsr=ngsr, B=B, pFunc=pFunc)
     benchRes <- benchmark(bench)
     print(xyplot(benchRes))
     return(invisible(benchRes))
 }
-##----------------------------------------##
-## test
-##----------------------------------------##
 tt <- newTwoGroupExprsSimulator(tpGeneSetCor=0.1)
 
+## quick tests
+fastTestSimulator <- function(simulator, pFunc) testSimulator(simulator, pFunc, ngsr=2, B=5)
+system.time(fastCameraTest <- fastTestSimulator(tt, pFunc=pFuncCamera))
+system.time(fastBioQCTest <- fastTestSimulator(tt, pFunc=pFuncBioQCtStat))
+system.time(fastCameraRankTest <- fastTestSimulator(tt, pFunc=pFuncCameraRank))
+system.time(fastFisherMethod <- fastTestSimulator(tt, pFunc=pFuncFisherMethod))
+system.time(fastMroastTest <- fastTestSimulator(tt, pFunc=pFuncMroast))
+system.time(fastFisherExact <- fastTestSimulator(tt, pFunc=pFuncFisherExact))
+system.time(fastGlobalTest <- fastTestSimulator(tt, pFunc=pFuncGlobaltest))
+system.time(fastTstatZtest <- fastTestSimulator(tt, pFunc=pFuncTstatZtest))
+system.time(fastTstatTtest <- fastTestSimulator(tt, pFunc=pFuncTstatTtest))
+system.time(fastTstatWMW <- fastTestSimulator(tt, pFunc=pFuncTstatWMW))
+system.time(fastTstatChisq <- fastTestSimulator(tt, pFunc=pFuncTstatChisq))
+system.time(fastRomer <- fastTestSimulator(tt, pFunc=pFuncRomer))
+
+##----------------------------------------##
+## Performance
+##----------------------------------------##
 cameraBenchmarkResult <- testSimulator(tt, pFuncCamera)
 bioqcBenchmarkResult <- testSimulator(tt, pFuncBioQCtStat)
 (rocCamera <- xyplot(cameraBenchmarkResult))
@@ -52,10 +68,10 @@ ttMutPos <- randomlyMutateBg(tt, bgDgePerc=0.3,
                              bgDgeDeltaMeanFunc=function(n) rnorm(n, mean=1, sd=1))
 ttMutPosCamera <- testSimulator(ttMutPos, pFunc=pFuncCamera)
 ttMutPosBioQC <- testSimulator(ttMutPos, pFunc=pFuncBioQCtStat)
-update(rocCamera, sub="CAMERA:Blue; BioQC: Red. Solid/Dash: iid/20% with logFC~N(1,1)", main="30% DE in background genes") + update(rocBioQC, col="red") + xyplot(ttMutPosCamera, lty=2, col=trellisPar$superpose.line$col[1]) + xyplot(ttMutPosBioQC, col="red", lty=2)
+update(rocCamera, sub="CAMERA:Blue; BioQC: Red. Solid/Dash: iid/30% with logFC~N(1,1)", main="30% DE in background genes") + update(rocBioQC, col="red") + xyplot(ttMutPosCamera, lty=2, col=trellisLineCols[1]) + xyplot(ttMutPosBioQC, col="red", lty=2)
 ipdf(figfile("ROC-randomDE.pdf"), width=5L, height=5L)
 
-update(rocCamera, sub="CAMERA. Solid/Dash: iid/30% with logFC~N(1,1)", main="DE in background genes")+xyplot(ttMutPosCamera, lty=2, col=trellisPar$superpose.line$col[1])
+update(rocCamera, sub="CAMERA. Solid/Dash: iid/30% with logFC~N(1,1)", main="DE in background genes")+xyplot(ttMutPosCamera, lty=2, col=trellisLineCols[1])
 ipdf(figfile("ROC-randomDE-cameraOnly.pdf"), width=5L, height=5L)
 
 update(rocBioQC, sub="BioQC. Solid/Dash: iid/30% with logFC~N(1,1)", main="DE in background genes", col="red")+xyplot(ttMutPosBioQC, lty=2, col="red")
@@ -68,34 +84,34 @@ ttMutMildPosCamera <- testSimulator(ttMutMildPos, pFunc=pFuncCamera)
 xyplot(ttMutMildPosCamera)
 
 ## if random gene sets are correlated
-testMutCor <- function(tgSim, pFunc=pFuncCamera) {
+testMutCor <- function(tgSim, pFunc=pFuncCamera, sigma=0.25) {
     bench <- newBenchmarker(tgSim, pFunc=pFunc)
     gsIndList <- bench@genesets[-1]
     gsInd <- unlist(gsIndList)
     gsFac <- factor(rep(seq(along=gsIndList), sapply(gsIndList,length)))
-    tgSimMut <- mutateBgByParams(tgSim, bgCorInd=gsInd, bgCorCluster=gsFac, bgCorSigma=0.5)
+    tgSimMut <- mutateBgByParams(tgSim, bgCorInd=gsInd, bgCorCluster=gsFac, bgCorSigma=sigma)
     bench <- newBenchmarker(tgSimMut, pFunc=pFunc, geneSets=genesets(bench))
     benchRes <- benchmark(bench)
     print(xyplot(benchRes))
     return(invisible(benchRes))
 }
-(rocMut3 <- testMutCor(tt))
-(rocMut3bioqc <- testMutCor(tt, pFunc=pFuncBioQCtStat))
+rocMut3 <- testMutCor(tt, pFunc=pFuncCamera)
+rocMut3bioqc <- testMutCor(tt, pFunc=pFuncBioQCtStat)
 
-
-xyplot(rocMut3, main="Strong correlation (0.5) in GS_R", sub="CAMERA:Blue; BioQC: Red. Solid/Dash: iid/50% cor in GS_R", lty=2)+xyplot(rocMut3bioqc, col="red", lty=2)+update(rocBioQC, col="red")+rocCamera
+xyplot(rocMut3, main="Moderate correlation (0.25) in GS_R", sub="CAMERA:Blue; BioQC: Red. Solid/Dash: iid/25% cor in GS_R", lty=2)+xyplot(rocMut3bioqc, col="red", lty=2)+update(rocBioQC, col="red")+rocCamera
 ipdf(figfile("ROC-GS_Rcor.pdf"), width=5L, height=5L)
 
-xyplot(rocMut3, main="Strong correlation (0.5) in GS_R", sub="CAMERA. Solid/Dash: iid/50% cor in GS_R", lty=2)+rocCamera
+xyplot(rocMut3, main="Strong correlation (0.25) in GS_R", sub="CAMERA. Solid/Dash: iid/25% cor in GS_R", lty=2)+rocCamera
 ipdf(figfile("ROC-GS_Rcor-cameraOnly.pdf"), width=5L, height=5L)
 
-update(rocBioQC, col="red", main="Strong correlation (0.5) in GS_R", sub="BioQC. Solid/Dash: iid/50% cor in GS_R")+xyplot(rocMut3bioqc, col="red", lty=2)
+update(rocBioQC, col="red", main="Strong correlation (0.25) in GS_R", sub="BioQC. Solid/Dash: iid/25% cor in GS_R")+xyplot(rocMut3bioqc, col="red", lty=2)
 ipdf(figfile("ROC-GS_Rcor-BioQConly.pdf"), width=5L, height=5L)
 
 AUC(rocMut3bioqc) ## AUC=.9
-summary(ranks(rocMut3bioqc)<=5) ## in 90% case ranks highest
+summary(ranks(rocMut3bioqc)<=5) ## in 88% case ranks highest
+summary(ranks(rocMut3)<=5) ## in 92% case ranks highest
 
-## as expected, if the expression of GS_R are also changed, the curve will look much worsex
+## as expected, if the expression of GS_R are also changed, the curve will look much worse
 testMutDeltaMean <- function(tgSim, pFunc=pFuncCamera) {
     bench <- newBenchmarker(tgSim, pFunc=pFunc)
     gsIndList <- bench@genesets[-1]
@@ -147,7 +163,7 @@ if(!loadFile(cameraBenchmarkFile)) {
                                })
     cameraOppESPerformance <- varParPerformance(varPar="deltaMean",
                                                 varParList=testOppDeltaMean, 
-                                                pFunc=pFuncCamera, tpGeneSetCor=0.1)
+                                                pFunc=pFuncCamera, tpGeneSetCor=0)
     
     testPartDeltaMeanVal <- seq(0.1, 0.9, 0.1)
     testPartDeltaMean <- lapply(testPartDeltaMeanVal,
@@ -197,8 +213,6 @@ ipdf(figfile("camera-Ranks-geneSetSize.pdf"), width=5, height=5)
 
 
 ## test effect size
-plotROC(cameraESPerformance, 
-        main="Camera's performance", key.title="Effect size", values=testDeltaMean)
 esCol <- c(royalbluered(3)[1],"lightgray", royalbluered(3)[3])
 plotROC(cameraESPerformance, main="Camera's performance", key.title="Effect size", values=testDeltaMean, cols=esCol)
 ipdf(figfile("camera-ROC-effectSize.pdf"), width=6.5, height=6L)
@@ -245,74 +259,56 @@ ipdf(figfile("BioQC.log10P.Tstat-ROC-cor.pdf"), width=6.5, height=6L)
 xyplot(sapply(bioqcTstatCorPerformance, AUC)~sapply(cameraCorPerformance, AUC), type="b", abline=c(0,1))
 ipdf(figfile("ROC-correlation-BioQC.log10P.Tstat-Camera.pdf"), width=6.5, height=6L)
 
-
-
-## camera ~ 14s
-(cameraSpeed <- system.time(testCameraPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncCamera)))
-
-## BioQC ~ 14s
-(bioqcSpeed <- system.time(testBioQCTstatPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncBioQCtStat)))
-
+## 100 runs' time
 ## cameraRank ~ 75s
-(cameraRankSpeed <- system.time(cameraRankCorPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncCameraRank)))
-
 ## fisher's method ~ 99s
-system.time(fisherMethodCorPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncFisherMethod))
-
 ## mroast ~ 126s 
-(mroastSpeed <- system.time(mroastCorPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncMroast)))
-
 ## fisher: 131s
-(fisherExactSpeed <- system.time(testFisherPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncFisherExact)))
-
 ## globaltest 290s
-(gtSpeed <- system.time(gtCorPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncGlobaltest)))
-
-
 ## one-sample z-test of t-statistic: 122s
-system.time(tStatZtestCorPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncTstatZtest))
-
 ## two-sample t-test of t-statistic: 144s
-(tStatTtestSpeed <- system.time(tStatTtestCorPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncTstatTtest)))
-
 ## WMW of t-statistic: 112s
-(tStatWmwSpeed <- system.time(tStatWmwCorPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncTstatWMW)))
-
 ## Chisq^test of t-statistic: 110s
-(chisqSpeed <- system.time(chisqCorPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncChisq)))
-
 
 ## test pFuncLimmaAggregated ~ 180s
-## system.time(pFuncLimmaAggregated(tt, list(1:20, 21:40)))
 
 ## romer ~ 336s
-system.time(romerCorPerformance <- varParPerformance(varPar="tpGeneSetCor", varParList=0.1, pFunc=pFuncRomer))
-
-## two-sample t-test of t-statistic
-## TODO
-## (1) Script
-## (2) GSEA
 
 
-## generate data
-biosBenchmarkData <- function(nSamples,
-                              deltaMean,
-                              tpGeneSetCor,
-                              tpGeneSetInd,
-                              bgDgePerc) {
-    obj <- newTwoGroupExprsSimulator(nSamples=nSamples,
-                                     deltaMean=deltaMean,
-                                     tpGeneSetCor=tpGeneSetCor,
-                                     tpGeneSetInd=tpGeneSetInd)
-    obj <- randomlyMutateBg(obj, bgDgePerc=bgDgePerc)
-    return(obj)
-                              
-}
-biosNs <- 2:6
-biosDeltas <- c(0.5, 1, 1.5, 2, 3)
-biosRos <- c(0, 0.1, 0.2, 0.5)
-biosThetas <- c(5, 10, 20, 50, 100)
-biosEpsilon <- c(0.01, 0.05, 0.25)
+##----------------------------------------##
+## simulate data
+##----------------------------------------##
+nSamples <- c(2:6,8,10)
+delta <- c(0.5, 1, 1.5, 2, 3)
+rou <- c(0, 0.1, 0.2, 0.5)
+theta <- c(2,5,10,20,50,100)
+epsilon <- c(0.005, 0.01, 0.05, 0.15)
 
-system.time(test <- biosBenchmarkData(2, 0.5, 0, 3, 0))
+dim(parameters <- expand.grid(nSample=nSamples, delta=delta, rou=rou, theta=theta, epsilon=epsilon))
+outdir <- "/data64/bi/data/gsea"
+outfileBases <- with(parameters, sprintf("n_%d_delta_%1.1f_rou_%1.1f_theta_%d_epsilon_%1.3f",
+                                         nSamples, delta, rou, theta, epsilon))
 
+comm <- with(parameters,
+             sprintf("/homebasel/biocomp/zhangj83/projects/2016-01-NEWS-ScienceAtTTB/gsa-v0.2/gsa-benchmark.Rscript -nSamples %d -deltaMean %.2f -tpGeneSetCor %.2f -tpGeneSetLength %d -bgDgePerc %.2f -outfile %s -statOutfile %s -nGSr 149 -B 100 -log %s",
+                     nSample, delta, rou, theta, epsilon,
+                     file.path(outdir,paste(outfileBases, ".RData", sep="")),
+                     file.path(outdir, paste(outfileBases, ".stat.txt", sep="")),
+                     file.path(outdir, paste(outfileBases, ".log", sep=""))))
+
+length(comm) ## 3360
+
+jobs <- sprintf("/apps64/bi/jobsubmitter/submit_job -command \"%s\" -name %s -dir %s -qsubFile qsub-GSEsimulation.bash",
+                comm, outfileBases, outdir)
+file.remove(c("prepareGSEsimulationJobs.bash", "qsub-GSEsimulation.bash"))
+writeLines(jobs, "prepareGSEsimulationJobs.bash")
+
+## bash prepareGSEsimulationJobs.bash
+##  ssh zhangj83@rbalhpc05 ~/projects//2016-01-NEWS-ScienceAtTTB/gsa-v0.2/qsub-GSEsimulation.bash
+
+## random gene sets' correlation
+mat <- ribiosGSEA:::rnormMatrix(1000, 6)
+rGeneSetSizes <- as.integer(runif(1000, min=1, max=200))
+rGeneSets <- lapply(1:1000, function(x) sample(1:nrow(mat), rGeneSetSizes[x]))
+matCamera <- limma::camera(mat, rGeneSets, design=cbind(rep(1,6), c(rep(0,3), rep(1,3))), contrast=2)
+with(matCamera, smoothScatter(Correlation ~ NGenes))
